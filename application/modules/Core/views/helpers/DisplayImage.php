79e212445f7018036c6f945874f4a349859c931b
boukrou <jseude@gmail.com>
2014-05-09 18:23:17 +0200

    Ajout des données structurées sur les fiches BD. fix #22

--- application/modules/Core/views/helpers/DisplayImage.php
+++ application/modules/Core/views/helpers/DisplayImage.php
@@ -5,19 +5,18 @@ class Zend_View_Helper_DisplayImage
     /*
      * 
      */
-    public function displayImage( $imgUri, $text="", $class="")
+    public function displayImage( $imgUri, $text="", $class="", $attr = '')
     {
         
         $loaderUri                          = BASE_URL . "img/loader.gif" ;
         $imgUri                             = $this->checkImage($imgUri) ;
         #list($width, $height, $type, $attr)= getimagesize($imgUri);
 
-        $attr = ''; 
         $text = str_replace(array('"',"'","`"), '', str_replace(array('<br>', '<br/>'), ' ', $text));
         #var_dump(is_resource($imgUri));
         
         if( $imgUri != false ){
-            return '<img class="lazy '.$class.'" src="'.$loaderUri.'" data-original="'.$imgUri.'" alt="'.$text.'" title="'.$text.'" '.$attr.' />
+            return '<img class="lazy '.$class.'" src="'.$loaderUri.'" data-original="'.$imgUri.'" alt="'.$text.'" title="'.$text.'" />
                 <noscript><img src="'.$imgUri.'" alt="'.$text.'" title="'.$text.'" '.$attr. '></noscript>'; 
         }
         
