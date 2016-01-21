7ea635c4215dc17f3031679bf9462095ffbda843
boukrou <jseude@gmail.com>
2016-01-22 00:58:03 +0100

    Repasse sur les url des auteurs.

--- application/modules/Core/controllers/AuteurController.php
+++ application/modules/Core/controllers/AuteurController.php
@@ -6,9 +6,14 @@ class AuteurController extends GlobalController
 {
     const ITEM_PER_PAGE = 20;
 
+    private $auteurMapper;
+    private $auteur;
+    private $id;
+
     public function init()
     {
         parent::init();
+        $this->auteurMapper = new Core_Model_Mapper_Tblauteurs();
         $this->view->headTitle('Auteurs BD - ', 'PREPEND');
     }
 
@@ -24,9 +29,8 @@ class AuteurController extends GlobalController
             $this->redirect301($this->view->url(array('letter' => $this->view->letter, 'page' => $this->view->page), 'listauteur_letter'));
         }
 
-        $auteur              = new Core_Model_Mapper_Tblauteurs();
-        $a                   = $auteur->fetchAll(null, 0, 'nomAuteur LIKE \''. $this->view->letter.'%\'', 'nomAuteur ASC', false, true);
-        $this->view->auteurs = $auteur->fetchAll(self::ITEM_PER_PAGE, self::ITEM_PER_PAGE * ($this->view->page - 1), 'nomAuteur LIKE \''. $this->view->letter.'%\'', 'nomAuteur ASC', true);
+        $a                   = $this->auteurMapper->fetchAll(null, 0, 'nomAuteur LIKE \''. $this->view->letter.'%\'', 'nomAuteur ASC', false, true);
+        $this->view->auteurs = $this->auteurMapper->fetchAll(self::ITEM_PER_PAGE, self::ITEM_PER_PAGE * ($this->view->page - 1), 'nomAuteur LIKE \''. $this->view->letter.'%\'', 'nomAuteur ASC', true);
 
         $this->view->paginator = Zend_Paginator::factory($a);
         $this->view->paginator->setCurrentPageNumber($this->view->page);
@@ -39,159 +43,144 @@ class AuteurController extends GlobalController
         }
     }
 
+    private function _get()
+    {
+        $this->id = $this->getRequest()->getParam('idAuteur');
+
+        if(!empty($this->id)) {
+            $this->auteur = $this->auteurMapper->find((int)$this->getRequest()->getParam('idAuteur'), new Core_Model_Tblauteurs);
+
+            if (!empty($this->auteur)) {
+                $good_url = $this->view->getHelper('customUrl')->getAuteurUrl($this->auteur);
+                if ($this->getRequest()->getRequestUri() != $good_url) {
+                    $this->redirect301($good_url);
+                }
+                return true;
+            }
+        }
+        throw new Zend_Controller_Action_Exception('Cet auteur n\'existe pas', 404);
+    }
+
     public function indexAction()
     {
-        $idAuteur = $this->getRequest()->getParam('idAuteur');
-        
-        $utils = new Core_Service_Utilities;
-        
-        $auteurMapper = new Core_Model_Mapper_Tblauteurs;
-        $auteurInfos  = $auteurMapper->find($idAuteur, new Core_Model_Tblauteurs);
-        
-        if($auteurInfos == null){
-	        $this->_redirect(array('accueil'));
-        }
-        
-        $this -> view -> nomAuteur          = $auteurInfos->getPrenomAuteur() 
-                                              . ' ' . ucwords(strtolower($auteurInfos->getNomAuteur()));
-        $this -> view -> bio                = $auteurInfos->getBiographie();
-        $this -> view -> jobsAuteur         = $this -> getJobs($auteurInfos);
-        $this -> view -> albumsAuteur       = $auteurMapper->getAuteurAlbums($idAuteur);
-        $albumsAuteurPrices                 = $auteurMapper->getAuteurAlbumPrices($idAuteur);
-        
+        $this->_get();
+
+        $this->view->nomAuteur    = $this->auteur->getPrenomAuteur() . ' ' . ucwords(strtolower($this->auteur->getNomAuteur()));
+        $this->view->bio          = $this->auteur->getBiographie();
+        $this->view->jobsAuteur   = $this->getJobs($this->auteur);
+        $this->view->albumsAuteur = $this->auteurMapper->getAuteurAlbums($this->id);
+        $albumsAuteurPrices       = $this->auteurMapper->getAuteurAlbumPrices($this->id);
+
         $this->view->headTitle($this->view->nomAuteur.' - ', 'PREPEND');
         $this->view->headMeta()->setName('description', !empty($this->view->bio) ? substr(strip_tags($this->view->bio), 0 , 250).'...' : 'Découvrez '.$this->view->nomAuteur.' auteur BD sur Sceneario.com');
         $this->view->headMeta()->setName('keywords', $this->view->nomAuteur.', '.$this->view->jobsAuteur.','.implode(',', (array_map(create_function('$e', 'return $e->titre;'), $this->view->albumsAuteur))));
         $this->view->headMeta()->setProperty('og:title', $this->view->nomAuteur);
         $this->view->headMeta()->setProperty('og:description', !empty($this->view->bio) ? substr(strip_tags($this->view->bio), 0 , 250).'...' : 'Découvrez '.$this->view->nomAuteur.' auteur BD sur Sceneario.com');        
-        
-        #echo '<pre>';
-        #print_r($auteurInfos);
-        
+
         $albumPrices = array();
         foreach($albumsAuteurPrices as $albumsAuteurPrice){
             $albumPrice  = new stdClass();
-            $albumMapper = new Core_Model_Mapper_Tblalbum ;
+            $albumMapper = new Core_Model_Mapper_Tblalbum;
             $albumInfos  = $albumMapper->find($albumsAuteurPrice->idAlbum, new Core_Model_Tblalbum);
-            
-            $albumPrice->festival  = $albumsAuteurPrice->nomFestival . ' ' . $albumsAuteurPrice->annee ;
-            $albumPrice->prix      = $albumsAuteurPrice->nomPrix ;
+
+            $albumPrice->festival  = $albumsAuteurPrice->nomFestival . ' ' . $albumsAuteurPrice->annee;
+            $albumPrice->prix      = $albumsAuteurPrice->nomPrix;
             $albumPrice->serie     = $albumsAuteurPrice->collection;
-            $albumPrice->serie    .= $albumInfos->getTome() != 0
-                                                  ? '&nbsp;#' . $albumInfos->getTome() 
-                                                  : '' ;
-            $albumPrice->url       = $this->view->customUrl(array('titleSerie' => $albumInfos->getCollection() . ' ' . $albumInfos->getTome(),
-                                                                  'titleAlbum' => $albumInfos->getTitre(), 
-                                                                  'idAlbum'    => $albumInfos->getIdAlbum()), 'album' );        
+            $albumPrice->serie    .= $albumInfos->getTome() != 0 ? '&nbsp;#' . $albumInfos->getTome() : '';
+            $albumPrice->url       = $this->view->customUrl(
+                array(
+                    'titleSerie' => $albumInfos->getCollection() . ' ' . $albumInfos->getTome(),
+                    'titleAlbum' => $albumInfos->getTitre(),
+                    'idAlbum'    => $albumInfos->getIdAlbum()),
+                'album'
+            );
             $albumPrices[] = $albumPrice;
         }
-        
-         $auteurGallery    = $auteurMapper->getAuteurGallery($idAuteur);
-        # $auteurPreview    = $auteurMapper->getAuteurPreview($idAuteur);
-         $auteurExpos      = $auteurMapper->getAuteurExpos($idAuteur) ;
-         $auteurInterview  = $auteurMapper->getAuteurInterview($idAuteur);
-         
-         $blocDatas  = array();
-         foreach($auteurExpos as $auteurExpo){
-             $exposData = array('rubrique'   => 'Salons, expositions',
-                                'titre'      => $auteurExpo->titre,
-                                'sous_titre' => $auteurExpo->sousTitre,
-                                'text'       => $auteurExpo->date,
-                                'image'      => $auteurExpo->image,
-                                'id'         => $auteurExpo->idexpo,
-                                'lien'       => array('type'=> 'voir tout' , 
-                                                      'url' => $this->view->customUrl(array('title' => $auteurExpo->titre, 
-                                                                                            'idexpo' => $auteurExpo->idexpo), 'expo'),
-                                                      'tout_afficher' =>'' ),
-                                'couleur'    => 'white'
-             );
-             
-             $bloc = $this->view->partial('partials/block300.phtml', 
-                                       array('data' => $exposData));
-             $bloc = null;
-             #$blocDatas[] = $bloc;
+
+        $auteurGallery    = $this->auteurMapper->getAuteurGallery($this->id);
+        # $auteurPreview    = $this->auteurMapper->getAuteurPreview($this->id);
+        $auteurExpos      = $this->auteurMapper->getAuteurExpos($this->id);
+        $auteurInterview  = $this->auteurMapper->getAuteurInterview($this->id);
+
+        $blocDatas  = array();
+        foreach($auteurExpos as $auteurExpo){
+            $exposData = array(
+                'rubrique'   => 'Salons, expositions',
+                'titre'      => $auteurExpo->titre,
+                'sous_titre' => $auteurExpo->sousTitre,
+                'text'       => $auteurExpo->date,
+                'image'      => $auteurExpo->image,
+                'id'         => $auteurExpo->idexpo,
+                'couleur'    => 'white',
+                'lien'       => array(
+                    'type'          => 'voir tout',
+                    'url'           => $this->view->customUrl(array('title' => $auteurExpo->titre, 'idexpo' => $auteurExpo->idexpo), 'expo'),
+                    'tout_afficher' => ''
+                ),
+            );
+
+            $blocDatas[] = $this->view->partial('partials/block300.phtml', array('data' => $exposData));
          }
-         
-         
-         
-         foreach($auteurGallery as $gallery){
-             
-             #var_dump($gallery->typeDossier);
-             $rubrique     = strtolower($gallery->typeDossier) == 'carnet' ? 'Galeries' : 'Previews' ;
-             $routeList    = strtolower($gallery->typeDossier) == 'carnet' ? 'listgalerie' : 'listpreview' ;
-             $galleryData = array('rubrique' => $rubrique ,
-                                'titre'      => $gallery->titreDossier,
-                                'sous_titre' => $gallery->titreSommaire,
-                                'text'       => $gallery->titreSommaire,
-                                'image'      => $utils->getImages($gallery->lienImage, false),
-                                'id'         => $gallery->idDossier,
-                                'lien'       => array('type'=> 'voir tout' , 
-                                                      'url' => $this->view->customUrl(array('nom'=> $gallery->titreDossier, 
-                                                                                            'id' => $gallery->idDossier), 'galerie'),
-                                                      'tout_afficher' => $this->view->customUrl(array(), $routeList )),
-                                'couleur'    => 'white'
-             );
-             
-             $bloc = $this->view->partial('partials/block300.phtml', 
-                                           array('data' => $galleryData));
-             #$bloc = null;
-             $blocDatas[] = $bloc;
-         }
-         
-         
-         foreach($auteurInterview as $interview){
-             $interviewData = array('rubrique' => 'Interviews',
-                                'titre'      => $interview->titreInterview,
-                                'sous_titre' => $interview->soustitreInterview,
-                                'text'       => $interview->soustitreInterview,
-                                'image'      => 'http://images.sceneario.com/' . $interview->lienImage,
-                                'id'         => $interview->idInterview,
-                                'lien'       => array('type'=> 'lire la suite' , 
-                                                      'url' =>$this->view->customUrl(array('nom' => $interview->titreInterview, 
-                                                                                           'id' => $interview->idInterview), 'interview'),
-                                                      'tout_afficher' => $this->view->customUrl(array(), 'listinterview')),
-                                'couleur'    => 'white'
-             );
-             
-             $bloc = $this->view->partial('partials/block300.phtml', 
-                                           array('data' => $interviewData));
-             
-             $blocDatas[] = $bloc;
-         }
-         
-         #print_r($blocDatas);
-    
-        $this -> view -> blocDatas   = $blocDatas ;
-        $this -> view -> allAlbumPrice  = $albumPrices ;
-        
-       # echo '<pre>';
-        
-       # print_r($auteurExpos);
-       # print_r($auteurInterview);
-       # print_r($auteurGallery);
+
+        $utils = new Core_Service_Utilities;
+
+        foreach($auteurGallery as $gallery){
+            $rubrique     = strtolower($gallery->typeDossier) == 'carnet' ? 'Galeries' : 'Previews';
+            $routeList    = strtolower($gallery->typeDossier) == 'carnet' ? 'listgalerie' : 'listpreview';
+            $galleryData = array(
+                'rubrique' => $rubrique,
+                'titre'      => $gallery->titreDossier,
+                'sous_titre' => $gallery->titreSommaire,
+                'text'       => $gallery->titreSommaire,
+                'image'      => $utils->getImages($gallery->lienImage, false),
+                'couleur'    => 'white',
+                'id'         => $gallery->idDossier,
+                'lien'       => array(
+                    'type'          => 'voir tout',
+                    'url'           => $this->view->customUrl(array('nom' => $gallery->titreDossier, 'id' => $gallery->idDossier), 'galerie'),
+                    'tout_afficher' => $this->view->customUrl(array(), $routeList)
+                ),
+            );
+
+            $blocDatas[] = $this->view->partial('partials/block300.phtml', array('data' => $galleryData));
+        }
+
+        foreach($auteurInterview as $interview){
+            $interviewData = array(
+                'rubrique' => 'Interviews',
+                'titre'      => $interview->titreInterview,
+                'sous_titre' => $interview->soustitreInterview,
+                'text'       => $interview->soustitreInterview,
+                'image'      => $utils->getImages($interview->lienImage, true),
+                'id'         => $interview->idInterview,
+                'couleur'    => 'white',
+                'lien'       => array(
+                    'type'          => 'lire la suite',
+                    'url'           => $this->view->customUrl(array('nom' => $interview->titreInterview, 'id' => $interview->idInterview), 'interview'),
+                    'tout_afficher' => $this->view->customUrl(array(), 'listinterview')
+                ),
+            );
+
+            $blocDatas[] = $this->view->partial('partials/block300.phtml', array('data' => $interviewData));
+        }
+
+        $this->view->blocDatas     = $blocDatas;
+        $this->view->allAlbumPrice = $albumPrices;
     }
-    
-    private function getJobs($auteurInfos)
+
+    private function getJobs()
     {
-        $jobs  = '';
-        $jobs .= $auteurInfos->getScenariste()  == 'O' ? ' Scenariste'  : '' ;
-        $jobs .= $auteurInfos->getDessinateur() == 'O' ? ' Dessinateur' : '' ;
-        $jobs .= $auteurInfos->getColoriste()   == 'O' ? ' Coloriste'   : '' ;
-        
-        return trim(str_replace(' ' ,  ' / ' , $jobs), ' / ') ;
+        $jobs  = array();
+        if ($this->auteur->getScenariste() == 'O') {
+            $jobs[] = 'Scenariste';
+        }
+        if ($this->auteur->getDessinateur() == 'O') {
+            $jobs[] = 'Dessinateur';
+        }
+        if ($this->auteur->getColoriste() == 'O') {
+            $jobs[] = 'Coloriste';
+        }
+
+        return implode(' / ', $jobs);
     }
 }
-
-/*
- * prive
- * [1] => stdClass Object
-        (
-            [idAlbum] => 7061
-            [collection] => ZORN ET DIRNA
-            [annee] => 2007
-            [nomPrix] => Meilleur Scénar' du mois de Mai des Editions Soleil
-            [nomFestival] => Sceneario.com
-            [lienOpale] => 
-        )
-
- */
--- application/modules/Core/views/helpers/CustomUrl.php
+++ application/modules/Core/views/helpers/CustomUrl.php
@@ -58,6 +58,14 @@ class Zend_View_Helper_CustomUrl extends Zend_View_Helper_Url
         return $this->customUrl($urlParams, 'serie');
     }
 
+    public function getAuteurUrl($auteur) {
+        $urlParams = array(
+            'nomAuteur' => $auteur->getPrenomAuteur() . ' ' . $auteur->getNomAuteur(),
+            'idAuteur'  => $auteur->getIdAuteur()
+        );
+        return $this->customUrl($urlParams,'auteur');
+    }
+
     /*
      * @todo implementer generecité
      * @var array $urlOptions
@@ -74,7 +82,7 @@ class Zend_View_Helper_CustomUrl extends Zend_View_Helper_Url
                     ->addFilter(new Zend_Filter_StringTrim()); //supprime les espace au début et a la fin
 
         foreach ($urlOptions as $key => &$urlOption){
-            if ($key != 'isbn' && in_array($name, array('couverture', 'planche', 'album', 'serie'))) {
+            if ($key != 'isbn' && in_array($name, array('couverture', 'planche', 'album', 'serie', 'auteur'))) {
                 $accents   = array('$', '@', '€', 'À','Á','Â','Ã','Ä','Å','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ò','Ó','Ô','Õ','Ö', 'Ù','Ú','Û','Ü','Ý','à','á','â','ã','ä','å','ç','è','é','ê','ë','ì','í','î','ï','ð','ò','ó','ô','õ','ö','ù','ú','û','ü','ý','ÿ');
                 $sans      = array('S', 'A', 'E', 'A','A','A','A','A','A','C','E','E','E','E','I','I','I','I','O','O','O','O','O', 'U','U','U','U','Y','a','a','a','a','a','a','c','e','e','e','e','i','i','i','i','o','o','o','o','o','o','u','u','u','u','y','y');
                 $urlOption = str_replace($accents, $sans, $urlOption);
