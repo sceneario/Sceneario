ffa974c357f432581e560bb466e4ef47b8a598c5
boukrou <jseude@gmail.com>
2016-01-29 16:29:58 +0100

    Amélioration controlleur expo/interview/galerie

--- application/modules/Core/views/helpers/CustomUrl.php
+++ application/modules/Core/views/helpers/CustomUrl.php
@@ -82,7 +82,7 @@ class Zend_View_Helper_CustomUrl extends Zend_View_Helper_Url
                     ->addFilter(new Zend_Filter_StringTrim()); //supprime les espace au début et a la fin
 
         foreach ($urlOptions as $key => &$urlOption){
-            if ($key != 'isbn' && in_array($name, array('couverture', 'planche', 'album', 'serie', 'auteur'))) {
+            if (!in_array($key, array('isbn', 'id')) && in_array($name, array('couverture', 'planche', 'album', 'serie', 'auteur', 'expo', 'interview', 'galerie'))) {
                 $accents   = array('$', '@', '€', 'À','Á','Â','Ã','Ä','Å','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ò','Ó','Ô','Õ','Ö', 'Ù','Ú','Û','Ü','Ý','à','á','â','ã','ä','å','ç','è','é','ê','ë','ì','í','î','ï','ð','ò','ó','ô','õ','ö','ù','ú','û','ü','ý','ÿ');
                 $sans      = array('S', 'A', 'E', 'A','A','A','A','A','A','C','E','E','E','E','I','I','I','I','O','O','O','O','O', 'U','U','U','U','Y','a','a','a','a','a','a','c','e','e','e','e','i','i','i','i','o','o','o','o','o','o','u','u','u','u','y','y');
                 $urlOption = str_replace($accents, $sans, $urlOption);
