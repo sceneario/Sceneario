4de20e29870345ed0e6b0f656366e2a5ee742893
boukrou <jseude@gmail.com>
2016-02-01 14:59:35 +0100

    Cache journalier des articles du blog à fournir sur la home.

--- application/modules/Core/views/helpers/LastBlogPostBloc.php
+++ application/modules/Core/views/helpers/LastBlogPostBloc.php
@@ -10,30 +10,39 @@ class Zend_View_Helper_LastBlogPostBloc extends Zend_View_Helper_CustomUrl
      */
     public function lastBlogPostBloc($view, $itemNumber = 0)
     {
-        if ($view instanceof Zend_View && $c = @file_get_contents('http://www.sceneario.com/blog/feed/')) {
-            $c   = str_replace('content:encoded>', 'content>', $c);
-            $rss = simplexml_load_string($c);
+        $cacheId = 'blog_feed_'.$itemNumber.'_'.date('Y_m_d');
+        $cache   = new Core_Service_ScenearioCache();
+        $content = $cache->getCache($cacheId);
 
-            if (!empty($rss) && !empty($rss->channel) && !empty($rss->channel->item) && !empty($rss->channel->item[0])) {
+        if (empty($content)) {
+            if ($view instanceof Zend_View && $c = @file_get_contents('http://www.sceneario.com/blog/feed/')) {
+                $c   = str_replace('content:encoded>', 'content>', $c);
+                $rss = simplexml_load_string($c);
 
-                $lastItem = $rss->channel->item[$itemNumber];
-                preg_match('/src=[\'"]?([^\'" >]+)[\'" >]/', $lastItem->content, $lastItemImg);
+                if (!empty($rss) && !empty($rss->channel) && !empty($rss->channel->item) && !empty($rss->channel->item[0])) {
+                    $lastItem = $rss->channel->item[$itemNumber];
+                    preg_match('/src=[\'"]?([^\'" >]+)[\'" >]/', $lastItem->content, $lastItemImg);
 
-                if (!empty($lastItem)) {
-                    $datas = array(
-                        'rubrique'   => 'Actualités',
-                        'titre'      => $lastItem->title,
-                        'sous_titre' => '',
-                        'text'       => '',
-                        'image'      => $lastItemImg[1],
-                        'id'         => '',
-                        'lien'       => array('type' => 'Lire la suite', 'url'  => $lastItem->link, 'tout_afficher' => 'http://www.sceneario.com/blog'),
-                        'couleur'    => 'white',
-                    );
-                    return $view->partial('partials/block300.phtml', array('data' => $datas));
+                    if (!empty($lastItem)) {
+                        $datas = array(
+                            'rubrique'   => 'Actualités',
+                            'titre'      => $lastItem->title,
+                            'sous_titre' => '',
+                            'text'       => '',
+                            'image'      => $lastItemImg[1],
+                            'id'         => '',
+                            'lien'       => array('type' => 'Lire la suite', 'url'  => $lastItem->link, 'tout_afficher' => 'http://www.sceneario.com/blog'),
+                            'couleur'    => 'white',
+                        );
+
+                        return $cache->setCache($cacheId, $view->partial('partials/block300.phtml', array('data' => $datas)));
+                    }
                 }
             }
+
+            return null ;
         }
-        return null ;
+
+        return $content;
     }
 }
